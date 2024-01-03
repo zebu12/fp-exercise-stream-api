f9a377cd591ef7c1742b2724ecb1329ba41dc881
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-01-03 11:29:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stream Exercise
+
+## Open ``StreamExercise`` Class at this path ``src/test/java/se/lexicon/vxo/service``
---
+++ src/main/java/se/lexicon/vxo/App.java
@@ -0,0 +1,9 @@
+package se.lexicon.vxo;
+
+
+public class App 
+{
+    public static void main( String[] args ) {
+        // Nothing to do here. Take a look at the test scope instead.
+    }
+}
